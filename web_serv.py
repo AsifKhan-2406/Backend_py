8c8dd14e4915761e3c4c6103b384b45a87fb646f
AsifKhan-2406 <146152698+AsifKhan-2406@users.noreply.github.com>
2024-04-07 10:38:38 +0530

    web_serv.py

--- web_serv.py
+++ web_serv.py
@@ -48,4 +48,4 @@ class SignIn(Resource):
             return {'message': 'Invalid username or password'}, 401
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = False, host = "0.0.0.0")
